c6e402fb291c84aead4bc0e84bba31101ba30123
vovamargaryan23 <v.margaryan204@gmail.com>
2022-10-12 21:08:42 +0400

    feat: update User entity with adding lombok annotations

--- src/main/java/com/shopapi/shopapi/entity/User.java
+++ src/main/java/com/shopapi/shopapi/entity/User.java
@@ -1,6 +1,7 @@
 package com.shopapi.shopapi.entity;
 
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
@@ -15,4 +16,16 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long userId;
+
+    @Column(name = "first_name")
+    @NonNull
+    private String firstName;
+
+    @Column(name = "last_name")
+    @NonNull
+    private String lastName;
+
+    @Column(name = "role")
+    @NonNull
+    private Role role;
 }
