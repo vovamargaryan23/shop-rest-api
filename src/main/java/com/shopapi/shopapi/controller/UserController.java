51d76918484311950e0a1f0b94fb1f8c9f2a3928
vovamargaryan23 <v.margaryan204@gmail.com>
2022-10-13 00:47:24 +0400

    feat: add UserController class

---
+++ src/main/java/com/shopapi/shopapi/controller/UserController.java
@@ -0,0 +1,42 @@
+package com.shopapi.shopapi.controller;
+
+import com.shopapi.shopapi.entity.Product;
+import com.shopapi.shopapi.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+
+@RestController
+@RequestMapping("/")
+public class UserController {
+
+    @Autowired
+    private ProductService productService;
+
+    @Autowired
+    private Set<Product> cart;
+
+    @GetMapping("/products")
+    public List<Product> findAllProducts(){
+        return productService.findAll();
+    }
+
+    @GetMapping("/cart")
+    public Set<Product> getCart(){
+        return cart;
+    }
+
+    @PostMapping("/cart")
+    public void addToCart(@RequestBody Product product){
+        cart.add(product);
+    }
+
+    @DeleteMapping("/cart/{productId}")
+    public void removeFromCart(@PathVariable("productId") Long id){
+        cart.removeIf(s -> Objects.equals(s.getProductId(), id));
+    }
+
+}
