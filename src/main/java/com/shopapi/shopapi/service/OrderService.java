e82658cc5b71d0e790c46ae66fc9f84331d3bc96
vovamargaryan23 <v.margaryan204@gmail.com>
2022-10-13 00:22:07 +0400

    feat: add OrderService class

--- src/main/java/com/shopapi/shopapi/service/OrderService.java
+++ src/main/java/com/shopapi/shopapi/service/OrderService.java
@@ -1,7 +1,27 @@
 package com.shopapi.shopapi.service;
 
+import com.shopapi.shopapi.entity.Order;
+import com.shopapi.shopapi.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class OrderService {
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    public List<Order> findAll(){
+        return orderRepository.findAll();
+    }
+
+    public Optional<Order> getOrderbyId(Long orderId){
+        return Optional.ofNullable(orderRepository.findById(orderId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND)));
+    }
 }
