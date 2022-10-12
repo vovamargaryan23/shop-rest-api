d82b44b969857c2839c47a7a4981969e5c23802b
vovamargaryan23 <v.margaryan204@gmail.com>
2022-10-12 23:51:14 +0400

    feat: update Order entity fields

--- src/main/java/com/shopapi/shopapi/entity/Order.java
+++ src/main/java/com/shopapi/shopapi/entity/Order.java
@@ -1,4 +1,33 @@
 package com.shopapi.shopapi.entity;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
+@RequiredArgsConstructor
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long orderId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private Long userId;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Long productId;
+
+    @Column(name = "amount")
+    private Long amount;
+
+    @Column(name = "order_date")
+    private LocalDateTime date;
 }
