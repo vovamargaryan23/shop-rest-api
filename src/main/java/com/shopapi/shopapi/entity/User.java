a0b69377bc888f58acee466d7b63595509df86b1
vovamargaryan23 <v.margaryan204@gmail.com>
2022-10-12 20:59:01 +0400

    fix: update id field name in User entity to userId

--- src/main/java/com/shopapi/shopapi/entity/User.java
+++ src/main/java/com/shopapi/shopapi/entity/User.java
@@ -1,5 +1,18 @@
 package com.shopapi.shopapi.entity;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@RequiredArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long userId;
 }
