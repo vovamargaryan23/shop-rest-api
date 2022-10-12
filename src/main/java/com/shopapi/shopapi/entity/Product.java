b9dd8f942fa7420aca2f6f45ccce707387ea82f8
vovamargaryan23 <v.margaryan204@gmail.com>
2022-10-12 20:50:40 +0400

    feat: update Product entity with adding necessary fields

--- src/main/java/com/shopapi/shopapi/entity/Product.java
+++ src/main/java/com/shopapi/shopapi/entity/Product.java
@@ -1,4 +1,28 @@
 package com.shopapi.shopapi.entity;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(name = "products")
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "product_id", nullable = false)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "price")
+    private Long price;
+
+    @ManyToOne
+    @JoinColumn(name = "product_category_id")
+    private ProductCategory productCategory;
+
+    @Column(name = "count")
+    private Long count;
 }
