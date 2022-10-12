1c2bfb056778ad3cccc098ae6f198178de5c499a
vovamargaryan23 <v.margaryan204@gmail.com>
2022-10-12 23:57:29 +0400

    feat: add Repository annotation to Repositories

--- src/main/java/com/shopapi/shopapi/repository/OrderRepository.java
+++ src/main/java/com/shopapi/shopapi/repository/OrderRepository.java
@@ -2,6 +2,8 @@ package com.shopapi.shopapi.repository;
 
 import com.shopapi.shopapi.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OrderRepository extends JpaRepository<Long, Order> {
 }
--- src/main/java/com/shopapi/shopapi/repository/ProductRepository.java
+++ src/main/java/com/shopapi/shopapi/repository/ProductRepository.java
@@ -2,6 +2,8 @@ package com.shopapi.shopapi.repository;
 
 import com.shopapi.shopapi.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Long, Product> {
 }
