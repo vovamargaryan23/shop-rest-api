5ec8c4a75607faaa584299a58d67b91d7f89d386
vovamargaryan23 <v.margaryan204@gmail.com>
2022-10-12 20:57:03 +0400

    feat: update ProductCategory entity with adding necessary fields

--- src/main/java/com/shopapi/shopapi/entity/ProductCategory.java
+++ src/main/java/com/shopapi/shopapi/entity/ProductCategory.java
@@ -1,16 +1,25 @@
 package com.shopapi.shopapi.entity;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 
 @Entity
-@Data
 @Table(name = "product_categories")
+@Getter
+@Setter
+@RequiredArgsConstructor
 public class ProductCategory {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "category_id", nullable = false)
     private Long id;
+
+    @Column(name = "category_name",nullable = false)
+    @NonNull
+    private String name;
 
 }
