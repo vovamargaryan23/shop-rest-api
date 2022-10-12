632e550565edf13a98b7e4ac9621dc2b8b92c1a7
vovamargaryan23 <v.margaryan204@gmail.com>
2022-10-13 00:51:50 +0400

    feat: add AdminController class

---
+++ src/main/java/com/shopapi/shopapi/controller/AdminController.java
@@ -0,0 +1,41 @@
+package com.shopapi.shopapi.controller;
+
+import com.shopapi.shopapi.entity.Product;
+import com.shopapi.shopapi.service.OrderService;
+import com.shopapi.shopapi.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+
+    @Autowired
+    private ProductService productService;
+
+    @Autowired
+    private OrderService orderService;
+
+    @GetMapping("/products")
+    public List<Product> findAllProducts(){
+        return productService.findAll();
+    }
+
+    @PostMapping("/products")
+    public void addProduct(@RequestBody Product product){
+        productService.updateProduct(product);
+    }
+
+    @PatchMapping("/products")
+    public void updateProduct(@RequestBody Product product){
+        productService.updateProduct(product);
+    }
+
+    @DeleteMapping("/products/{productId}")
+    public void removeProduct(@PathVariable("productId") Long id){
+        productService.removeProductById(id);
+    }
+
+}
