6f74d718c8c05e2f6935e3b4e02b80ff9d6964f7
vovamargaryan23 <v.margaryan204@gmail.com>
2022-10-14 16:29:16 +0400

    feat: update UserService class

--- src/main/java/com/shopapi/shopapi/service/UserService.java
+++ src/main/java/com/shopapi/shopapi/service/UserService.java
@@ -1,7 +1,19 @@
 package com.shopapi.shopapi.service;
 
+import com.shopapi.shopapi.entity.User;
+import com.shopapi.shopapi.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User findById(Long userId){
+        return userRepository.findById(userId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
 }
