022b3848afa2cc5d7af41bccd3c79b921dece6e7
vovamargaryan23 <v.margaryan204@gmail.com>
2022-10-14 16:26:06 +0400

    feat: update UserRepository interface

--- src/main/java/com/shopapi/shopapi/repository/UserRepository.java
+++ src/main/java/com/shopapi/shopapi/repository/UserRepository.java
@@ -1,4 +1,9 @@
 package com.shopapi.shopapi.repository;
 
-public interface UserRepository {
+import com.shopapi.shopapi.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
 }
