6520bf93fd8825f563bf7facfe44b8dc51a97a27
vovamargaryan23 <v.margaryan204@gmail.com>
2022-10-14 22:48:27 +0400

    refactor: remove semicolon in enums

--- src/main/java/com/shopapi/shopapi/entity/OrderStatus.java
+++ src/main/java/com/shopapi/shopapi/entity/OrderStatus.java
@@ -1,5 +1,5 @@
 package com.shopapi.shopapi.entity;
 
 public enum OrderStatus {
-    PENDING,APPROVED,DECLINED;
+    PENDING,APPROVED,DECLINED
 }
--- src/main/java/com/shopapi/shopapi/entity/Role.java
+++ src/main/java/com/shopapi/shopapi/entity/Role.java
@@ -1,5 +1,5 @@
 package com.shopapi.shopapi.entity;
 
 public enum Role {
-    ROLE_ADMIN,ROLE_USER;
+    ROLE_ADMIN,ROLE_USER
 }
