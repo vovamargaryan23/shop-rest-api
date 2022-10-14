07ac9c7972a743565b26b5c883c371eea18c3a28
vovamargaryan23 <v.margaryan204@gmail.com>
2022-10-14 16:17:15 +0400

    fix: change MainController logic, now working with requests

--- src/main/java/com/shopapi/shopapi/controller/AdminController.java
+++ src/main/java/com/shopapi/shopapi/controller/AdminController.java
@@ -6,10 +6,13 @@ import com.shopapi.shopapi.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
+import java.util.Set;
 
 @RestController
-@RequestMapping("/admin")
+@RequestMapping("/{userId}")
 public class AdminController {
 
     @Autowired
@@ -18,10 +21,36 @@ public class AdminController {
     @Autowired
     private OrderService orderService;
 
+    private final Set<Product> cart;
+
+
+    public AdminController() {
+        this.cart = new HashSet<>();
+    }
+
+    //User Requests
     @GetMapping("/products")
     public List<Product> findAllProducts(){
         return productService.findAll();
     }
+
+    @GetMapping("/cart")
+    public Set<Product> getCart(){
+        return cart;
+    }
+
+    @PostMapping("/cart/{productId}")
+    public void addToCart(@PathVariable("productId") Long productId){
+        cart.add(productService.findById(productId));
+    }
+
+    @DeleteMapping("/cart/{productId}")
+    public void removeFromCart(@PathVariable("productId") Long id){
+        cart.removeIf(s -> Objects.equals(s.getProductId(), id));
+    }
+
+
+    //Admin Requests
 
     @PostMapping("/products")
     public void addProduct(@RequestBody Product product){
